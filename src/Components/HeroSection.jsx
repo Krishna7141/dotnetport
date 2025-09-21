cbbdb93534e079f6e48f399fade550b2b7ea2763
Krishna Vamsi Chelluboina <krishnavamsich2000@gmail.com>
2025-09-20 22:14:56 -0500

    Reduce top margin of signature image to bring it closer to photo

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -55,8 +55,7 @@ const HeroSection = () => {
                 {/* <img src={Blob} className='absolute z-50 h-[500px] -translate-x-5 translate-y-10 scale-150 opacity-80'/> */}
                 <img src={KrishImg} className='z-50 contrast-125 scale-95 transition-all duration-1000 max-h-[430px]'/>
                 <img src={signatureImg} alt="Signature"
-                     className="w-40 mx-auto mt-4 opacity-90 transition-opacity duration-700 hover:opacity-100" />
-
+                     className="w-40 mx-auto mt-2 opacity-90 drop-shadow-md transition-opacity duration-700 hover:opacity-100" />
             </div>
         </div>
 
